80dbdd779ab5d335bb32ff8d770278dce7b0678a
MihaiS-git <ingsuciumihai@gmail.com>
2025-07-07 12:48:00 +0300

    style on home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,9 +3,9 @@ import PageContent from "../components/PageContent";
 const HomePage = () => {
   return (
     <PageContent className="flex flex-col text-center items-center py-24 bg-blue-600">
-      <div className="text-4xl font-extrabold text-center p-4 m-auto">
+      <div className="text-4xl font-extrabold text-center p-4 m-auto text-blue-100 dark:text-gray-300">
         <h1>Job Matcher</h1>
-        <h2 className="text-lg font-extralight">The Freelancers Home</h2>
+        <h2 className="text-lg font-extralight">The Freelancers Home Page</h2>
       </div>
     </PageContent>
   );
