e7620e10cdd524ca8769612d5b7be4bbaf5d310b
MihaiS-git <ingsuciumihai@gmail.com>
2025-07-10 12:33:17 +0300

    add validation on the auth form

--- src/pages/auth/AuthPage.tsx
+++ src/pages/auth/AuthPage.tsx
@@ -4,26 +4,42 @@ import { useState } from "react";
 import { useLoginMutation } from "../../features/authApi";
 import { useAppDispatch } from "../../hooks/hooks";
 import { setCredentials } from "../../features/authSlice";
+import { validateEmail, validatePassword } from "../../utils/validation";
 
 const AuthPage = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [login] = useLoginMutation();
-    const dispatch = useAppDispatch();
-    const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [login, { isLoading }] = useLoginMutation();
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const [errors, setErrors] = useState<{ email?: string | null; password?: string | null}>();
 
+  const handleLoginFormSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
 
-    const handleLoginFormSubmit = async (e: React.FormEvent) => {
-        e.preventDefault();
-        try{
-        const result = await login({email, password}).unwrap();
-        dispatch(setCredentials({user: result.user, token: result.token, refreshToken: result.refreshToken}));
-        navigate("/");
-        } catch (err: unknown){
-          console.error("Login failed", err);
-          alert("Login failed. Please try again.")
-        }
+    const emailError = validateEmail(email);
+    const passwordError = validatePassword(password);
+
+    if (emailError || passwordError) {
+      setErrors({ email: emailError, password: passwordError });
+      return;
     }
+
+    try {
+      const result = await login({ email, password }).unwrap();
+      dispatch(
+        setCredentials({
+          user: result.user,
+          token: result.token,
+          refreshToken: result.refreshToken,
+        })
+      );
+      navigate("/");
+    } catch (err: unknown) {
+      console.error("Login failed", err);
+      alert("Login failed. Please try again.");
+    }
+  };
 
   return (
     <PageContent className="flex flex-col items-center justify-center">
@@ -31,7 +47,10 @@ const AuthPage = () => {
         <h1 className="font-semibold text-lg xl:text-2xl text-center m-8 mx-auto">
           Authentication Form
         </h1>
-        <form className="flex flex-col items-stretch p-4" onSubmit={(e) => handleLoginFormSubmit(e)}>
+        <form
+          className="flex flex-col items-stretch p-4"
+          onSubmit={(e) => handleLoginFormSubmit(e)}
+        >
           <label
             htmlFor="email"
             className="font-light text-sm xl:text-base px-4 w-full flex flex-row items-center justify-between"
@@ -41,11 +60,22 @@ const AuthPage = () => {
               id="email"
               type="email"
               name="email"
+              value={email}
               className="bg-gray-200 text-gray-950 px-2 py-0.5 m-2 w-4/6 rounded-sm border border-gray-950 text-sm xl:text-base"
               placeholder="your@email.com"
               onChange={(e) => setEmail(e.target.value)}
+              onBlur={() => {
+                if (email && errors?.email)
+                  setErrors((prev) => ({ ...prev, email: undefined }));
+              }}
             />
           </label>
+          {errors?.email && (
+            <p className="text-red-600 text-xs px-4 mt-[-8px] mb-2">
+              {errors.email}
+            </p>
+          )}
+
           <label
             htmlFor="password"
             className="font-light text-sm xl:text-base px-4 w-full flex flex-row items-center justify-between"
@@ -55,29 +85,47 @@ const AuthPage = () => {
               id="password"
               type="password"
               name="password"
+              value={password}
               className="bg-gray-200 text-gray-950 px-2 py-0.5 m-2 w-4/6 rounded-sm border border-gray-950 text-sm xl:text-base"
               placeholder="password"
               onChange={(e) => setPassword(e.target.value)}
+              onBlur={() => {
+                if (password && errors?.password) {
+                  setErrors((prev) => ({ ...prev, password: undefined }));
+                }
+              }}
             />
           </label>
+          {errors?.password && (
+            <p className="text-red-600 text-xs px-4 mt-[-8px] mb-2">
+              {errors.password}
+            </p>
+          )}
+
           <div className="w-full text-center py-8">
             <button
               type="submit"
               className="bg-blue-400 hover:bg-blue-600 dark:bg-blue-600 dark:hover:bg-blue-400 hover:text-blue-200 dark:hover:text-blue-950 px-12 py-1 rounded-sm ring-1 ring-blue-900 text-sm xl:text-base"
             >
-              Login
+              {isLoading ? "Logging in..." : "Login"}
             </button>
           </div>
           <div className="flex flex-col items-center justify-center">
             <p className="text-xs font-extralight">
               Don't you have an account?{" "}
-              <Link to="/register" className="text-blue-800 underline dark:text-blue-200 hover:text-red-600">
+              <Link
+                to="/register"
+                className="text-blue-800 underline dark:text-blue-200 hover:text-red-600"
+              >
                 Register
               </Link>
             </p>
             <p className="text-xs font-extralight">
               Forgot your password?{" "}
-              <Link to="/reset-password" className="text-blue-800 underline dark:text-blue-200 hover:text-red-600">
+              <Link
+                to="/reset-password"
+                className="text-blue-800 underline dark:text-blue-200 hover:text-red-600"
+              >
                 Reset Password
               </Link>
             </p>
