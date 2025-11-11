8983291eaac508421262e75359fd5961401f498d
MihaiS-git <ingsuciumihai@gmail.com>
2025-11-11 14:21:38 +0200

    add demo features

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,19 +2,25 @@ import { Link } from "react-router-dom";
 import ThemeToggleButton from "./ThemeToggleButton";
 import MainNavigation from "./MainNavigation";
 import Hamburger from "./Hamburger";
+import DemoComponent from "./DemoComponent";
 
 const Header = () => {
   return (
-    <header className="fixed top-0 left-0 right-0 h-16 z-40 w-full p-4 flex justify-between items-center xl:px-80 bg-blue-600 dark:bg-gray-900 text-blue-100 dark:text-gray-300">
-      <Link to="/" className="flex items-center">
-        <h1 className="text-xl lg:text-2xl font-extrabold hover:text-blue-950 dark:hover:text-blue-400 whitespace-nowrap">
-          Job Matcher
-        </h1>
-      </Link>
-      <MainNavigation />
-      <div className="flex flex-row gap-2">
-        <ThemeToggleButton />
-        <Hamburger />
+    <header className="">
+      <div className="flex flex-col">
+        <div className="fixed top-0 left-0 right-0 h-16 z-40 w-full p-4 flex justify-between items-center xl:px-80 bg-blue-600 dark:bg-gray-900 text-blue-100 dark:text-gray-300">
+          <Link to="/" className="flex items-center">
+            <h1 className="text-xl lg:text-2xl font-extrabold hover:text-blue-950 dark:hover:text-blue-400 whitespace-nowrap">
+              Job Matcher
+            </h1>
+          </Link>
+          <DemoComponent />
+          <MainNavigation />
+          <div className="flex flex-row gap-2">
+            <ThemeToggleButton />
+            <Hamburger />
+          </div>
+        </div>
       </div>
     </header>
   );
