87ecc0576dd390159b446e52c0a69f4779fb0679
MihaiS-git <ingsuciumihai@gmail.com>
2025-11-12 13:04:29 +0200

    update proposal details page ux

--- src/components/DemoComponent.tsx
+++ src/components/DemoComponent.tsx
@@ -75,14 +75,14 @@ const DemoComponents = () => {
     <section className="max-w-md p-1 mx-1 lg:ms-8 text-base bg-blue-400 dark:bg-gray-700 rounded-sm">
       <div className="max-w-md flex flex-col gap-1 justify-center align-middle items-center">
         <Button onClick={() => handleDemoLogin("freelancer")} variant="destructive" className="w-20 h-6 rounded-sm gap-0.5 px-1 has-[>svg]:px-0.5 text-xs animate-pulse" size="sm">
-          {isLoading ? (
+          {(isLoading || apiError) ? (
             <LoadingSpinner size={12} fullScreen={false} />
           ) : (
             "Freelancer"
           )}
         </Button>
         <Button onClick={() => handleDemoLogin("customer")} variant="destructive" className="w-20 h-6 rounded-sm gap-0.5 px-1 has-[>svg]:px-0.5 text-xs animate-pulse" size="sm">
-          {isLoading ? (
+          {(isLoading || apiError) ? (
             <LoadingSpinner size={12} fullScreen={false} />
           ) : (
             "Customer"
--- src/components/proposal/ProposalDetails.tsx
+++ src/components/proposal/ProposalDetails.tsx
@@ -12,6 +12,9 @@ import type { ProposalStatus } from "@/types/ProposalDTO";
 import useAuth from "@/hooks/useAuth";
 import { ProjectStatusLabels } from "@/types/formLabels/projectLabels";
 import { formatDate } from "@/utils/formatDate";
+import FeedbackMessage from "../FeedbackMessage";
+import { useEffect, useState } from "react";
+import { parseApiError } from "@/utils/parseApiError";
 
 type ProposalProps = {
   id: string;
@@ -21,12 +24,13 @@ const ProposalDetails = ({ id }: ProposalProps) => {
   const navigate = useNavigate();
   const auth = useAuth();
   const role = auth?.user?.role;
+  const [apiError, setApiError] = useState<string | null>(null);
 
   const {
     data: proposal,
     isLoading: isLoadingProposal,
     error: proposalError,
-  } = useGetProposalByIdQuery(id!);
+  } = useGetProposalByIdQuery(id ?? skipToken);
   const {
     data: project,
     isLoading: isProjectLoading,
@@ -36,8 +40,11 @@ const ProposalDetails = ({ id }: ProposalProps) => {
   const [updateProposal, { isLoading: isUpdating, error: updateError }] =
     useUpdateProposalByIdMutation();
 
-  const isLoading = isLoadingProposal || isProjectLoading || isUpdating;
-  const error = proposalError || projectError || updateError;
+  useEffect(() => {
+    if (proposalError || projectError || updateError) {
+      setApiError(parseApiError(proposalError || projectError || updateError));
+    }
+  }, [proposalError, projectError, updateError]);
 
   const navigateToEditProposal = () => {
     navigate(`/proposals/edit/${id}`);
@@ -50,204 +57,215 @@ const ProposalDetails = ({ id }: ProposalProps) => {
         updatedProposal: { status: "WITHDRAWN" as ProposalStatus },
       });
     } catch (e: unknown) {
-      console.error("Failed to withdraw proposal:", e);
+      setApiError(
+        parseApiError(e) ||
+          "Failed to withdraw proposal. Please try again later."
+      );
     }
     navigate("/proposals");
   };
 
   if (!id) {
-    return <div className="text-red-500">Invalid proposal ID.</div>;
+    return <FeedbackMessage message="Invalid proposal ID." type="error" />;
   }
 
   return (
     <div className="w-full sm:max-w-6xl p-4 mb-8 border border-gray-300 dark:border-gray-600 rounded-lg bg-white dark:bg-gray-800 shadow-md">
-      {isLoading && <LoadingSpinner fullScreen={false} size={24} />}
-      {error && (
-        <div>
-          Error loading proposal details:{" "}
-          {"status" in error
-            ? typeof error.data === "string"
-              ? error.data
-              : JSON.stringify(error.data)
-            : error.message || "Unknown error"}
-        </div>
+      {(proposalError || projectError || updateError) && (
+        <FeedbackMessage message={apiError!} type="error" />
       )}
-      {updateError && (
-        <div className="text-red-500">
-          Error updating proposal:{" "}
-          {"status" in updateError
-            ? typeof updateError.data === "string"
-              ? updateError.data
-              : JSON.stringify(updateError.data)
-            : updateError.message || "Unknown error"}
-        </div>
-      )}
-
-      {project ? (
-        <div className="w-full text-center border-b px-1 md:px-2 py-1 md:py-4">
-          <h2 className="text-blue-600 mb-4 text-center">
-            <b>Project Data</b>{" "}
-          </h2>
-
-          <section className="text-sm font-medium text-start grid grid-cols-3 lg:grid-cols-5 gap-1 md:gap-2">
-            <p>Project Title: </p>
-            <p className="font-light col-span-2 lg:col-span-4">
-              {project?.title}
-            </p>
-
-            <p>Project ID: </p>
-            <Link
-              to={`/projects/${proposal?.projectId}`}
-              className="underline text-blue-500 hover:text-blue-400 italic font-light col-span-2 lg:col-span-4"
-            >
-              {proposal?.projectId}
-            </Link>
-
-            <p>Customer: </p>
-            <Link
-              to={`/public_profile/customer/${project?.customer?.profileId}`}
-              className="underline text-blue-500 hover:text-blue-400 italic font-light col-span-2 lg:col-span-4"
-            >
-              {project?.customer?.username}
-            </Link>
-
-            <p>Description: </p>
-            <p className="font-light col-span-2 lg:col-span-4">
-              {project?.description}
-            </p>
-
-            <p>Status: </p>
-            <p className="font-light col-span-2 lg:col-span-4">
-              {ProjectStatusLabels[project.status!]}
-            </p>
-
-            <p className="pe-2">Budget: </p>
-            <p className="font-light col-span-2 lg:col-span-4">
-              {project?.budget ? formatCurrency(project.budget) : "N/A"}
-            </p>
-            <p className="pe-2">Payment Type: </p>
-            <p className="font-light col-span-2 lg:col-span-4">
-              {project?.paymentType || "N/A"}
-            </p>
-            <p className="pe-2">Deadline: </p>
-            <p className="font-light col-span-2 lg:col-span-4">
-              {project?.deadline ? formatDate(project.deadline) : "N/A"}
-            </p>
-            <p className="pe-2">Category: </p>
-            <p className="font-light col-span-2 lg:col-span-4">
-              {project?.category?.name || "N/A"}
-            </p>
-            <p className="pe-2">Subcategories: </p>
-            <p className="font-light col-span-2 lg:col-span-4">
-              {project?.subcategories && project.subcategories.length > 0
-                ? project.subcategories.map((sub) => sub.name).join(", ")
-                : "N/A"}
-            </p>
-            <p className="pe-2">Created at: </p>
-            <p className="font-light col-span-2 lg:col-span-4">
-              {project?.createdAt ? formatDate(project.createdAt) : "N/A"}
-            </p>
-            <p className="pe-2">Last update: </p>
-            <p className="font-light col-span-2 lg:col-span-4">
-              {project?.lastUpdate ? formatDate(project.lastUpdate) : "N/A"}
-            </p>
-          </section>
-        </div>
+      {!proposal ? (
+        <LoadingSpinner fullScreen={false} size={36} />
       ) : (
-        <div className="text-red-500">Project data not available.</div>
+        <>
+          {project ? (
+            <div className="w-full text-center border-b px-1 md:px-2 py-1 md:py-4">
+              <h2 className="text-blue-600 mb-4 text-center">
+                <b>Project Data</b>{" "}
+              </h2>
+
+              <section className="text-sm font-medium text-start grid grid-cols-3 lg:grid-cols-5 gap-1 md:gap-2">
+                <p>Project Title: </p>
+                <p className="font-light col-span-2 lg:col-span-4">
+                  {project?.title}
+                </p>
+
+                <p>Project ID: </p>
+                <Link
+                  to={`/projects/${proposal?.projectId}`}
+                  className="underline text-blue-500 hover:text-blue-400 italic font-light col-span-2 lg:col-span-4"
+                >
+                  {proposal?.projectId}
+                </Link>
+
+                <p>Customer: </p>
+                <Link
+                  to={`/public_profile/customer/${project?.customer?.profileId}`}
+                  className="underline text-blue-500 hover:text-blue-400 italic font-light col-span-2 lg:col-span-4"
+                >
+                  {project?.customer?.username}
+                </Link>
+
+                <p>Description: </p>
+                <p className="font-light col-span-2 lg:col-span-4">
+                  {project?.description}
+                </p>
+
+                <p>Status: </p>
+                <p className="font-light col-span-2 lg:col-span-4">
+                  {ProjectStatusLabels[project.status!]}
+                </p>
+
+                <p className="pe-2">Budget: </p>
+                <p className="font-light col-span-2 lg:col-span-4">
+                  {project?.budget ? formatCurrency(project.budget) : "N/A"}
+                </p>
+                <p className="pe-2">Payment Type: </p>
+                <p className="font-light col-span-2 lg:col-span-4">
+                  {project?.paymentType || "N/A"}
+                </p>
+                <p className="pe-2">Deadline: </p>
+                <p className="font-light col-span-2 lg:col-span-4">
+                  {project?.deadline ? formatDate(project.deadline) : "N/A"}
+                </p>
+                <p className="pe-2">Category: </p>
+                <p className="font-light col-span-2 lg:col-span-4">
+                  {project?.category?.name || "N/A"}
+                </p>
+                <p className="pe-2">Subcategories: </p>
+                <p className="font-light col-span-2 lg:col-span-4">
+                  {project?.subcategories && project.subcategories.length > 0
+                    ? project.subcategories.map((sub) => sub.name).join(", ")
+                    : "N/A"}
+                </p>
+                <p className="pe-2">Created at: </p>
+                <p className="font-light col-span-2 lg:col-span-4">
+                  {project?.createdAt ? formatDate(project.createdAt) : "N/A"}
+                </p>
+                <p className="pe-2">Last update: </p>
+                <p className="font-light col-span-2 lg:col-span-4">
+                  {project?.lastUpdate ? formatDate(project.lastUpdate) : "N/A"}
+                </p>
+              </section>
+            </div>
+          ) : (
+            <>
+              {isProjectLoading ? (
+                <LoadingSpinner fullScreen={false} size={24} />
+              ) : (
+                <FeedbackMessage
+                  message="Project data not available."
+                  type="error"
+                />
+              )}
+            </>
+          )}
+
+          {proposal ? (
+            <div className="w-full text-center border-b px-1 md:px-2 py-1 md:py-4">
+              <h2 className="text-blue-600 mb-4 text-center">
+                <b>Proposal Data</b>
+              </h2>
+
+              <section className="text-sm font-medium text-start grid grid-cols-3 lg:grid-cols-5 gap-1 md:gap-2">
+                <p className="pe-2">Freelancer: </p>
+                <Link
+                  to={`/public_profile/freelancer/${proposal?.freelancer?.profileId}`}
+                  className="underline text-blue-500 hover:text-blue-400 italic font-light col-span-2 lg:col-span-4"
+                >
+                  {proposal?.freelancer?.username}
+                </Link>
+
+                <p className="pe-2">Cover Letter:</p>
+                <p className="font-light col-span-2 lg:col-span-4">
+                  {proposal?.coverLetter}
+                </p>
+
+                <p>Proposed amount: </p>
+                <p className="font-light col-span-2 lg:col-span-4">
+                  {formatCurrency(proposal?.amount || 0)}
+                </p>
+
+                <p>Penalty amount: </p>
+                <p className="font-light col-span-2 lg:col-span-4">
+                  {formatCurrency(proposal?.penaltyAmount || 0)}
+                </p>
+
+                <p className="pe-2">Bonus amount: </p>
+                <p className="font-light col-span-2 lg:col-span-4">
+                  {formatCurrency(proposal?.bonusAmount || 0)}
+                </p>
+
+                <p className="pe-2">Estimated duration: </p>
+                <p className="font-light col-span-2 lg:col-span-4">
+                  {proposal?.estimatedDuration} days
+                </p>
+
+                <p className="pe-2">Status: </p>
+                <p className="font-light col-span-2 lg:col-span-4">
+                  {proposal?.status}
+                </p>
+
+                <p className="pe-2">Planned Start Date:</p>
+                <p className="font-light col-span-2 lg:col-span-4">
+                  {proposal?.plannedStartDate
+                    ? formatDate(proposal.plannedStartDate)
+                    : "N/A"}
+                </p>
+
+                <p className="pe-2">Planned End Date:</p>
+                <p className="font-light col-span-2 lg:col-span-4">
+                  {proposal?.plannedEndDate
+                    ? formatDate(proposal.plannedEndDate)
+                    : "N/A"}
+                </p>
+
+                <p className="pe-2">Actual Start Date:</p>
+                <p className="font-light col-span-2 lg:col-span-4">
+                  {proposal?.actualStartDate
+                    ? formatDate(proposal.actualStartDate)
+                    : "N/A"}
+                </p>
+
+                <p className="pe-2">Actual End Date:</p>
+                <p className="font-light col-span-2 lg:col-span-4">
+                  {proposal?.actualEndDate
+                    ? formatDate(proposal.actualEndDate)
+                    : "N/A"}
+                </p>
+
+                <p className="pe-2">Created At: </p>
+                <p className="font-light col-span-2 lg:col-span-4">
+                  {proposal?.createdAt ? formatDate(proposal.createdAt) : "N/A"}
+                </p>
+
+                <p className="pe-2">Last Update: </p>
+                <p className="font-light col-span-2 lg:col-span-4">
+                  {proposal?.lastUpdate
+                    ? formatDate(proposal.lastUpdate)
+                    : "N/A"}
+                </p>
+
+                <p className="pe-2">Notes:</p>
+                <p className="font-light col-span-2 lg:col-span-4">
+                  {proposal?.notes}
+                </p>
+              </section>
+            </div>
+          ) : (
+            <>
+              {isLoadingProposal ? (
+                <LoadingSpinner fullScreen={false} size={24} />
+              ) : (
+                <FeedbackMessage
+                  message="Proposal data not available."
+                  type="error"
+                />
+              )}
+            </>
+          )}
+        </>
       )}
-
-      {proposal && (
-        <div className="w-full text-center border-b px-1 md:px-2 py-1 md:py-4">
-          <h2 className="text-blue-600 mb-4 text-center">
-            <b>Proposal Data</b>
-          </h2>
-
-          <section className="text-sm font-medium text-start grid grid-cols-3 lg:grid-cols-5 gap-1 md:gap-2">
-            <p className="pe-2">Freelancer: </p>
-            <Link
-              to={`/public_profile/freelancer/${proposal?.freelancer?.profileId}`}
-              className="underline text-blue-500 hover:text-blue-400 italic font-light col-span-2 lg:col-span-4"
-            >
-              {proposal?.freelancer?.username}
-            </Link>
-
-            <p className="pe-2">Cover Letter:</p>
-            <p className="font-light col-span-2 lg:col-span-4">
-              {proposal?.coverLetter}
-            </p>
-
-            <p>Proposed amount: </p>
-            <p className="font-light col-span-2 lg:col-span-4">
-              {formatCurrency(proposal?.amount || 0)}
-            </p>
-
-            <p>Penalty amount: </p>
-            <p className="font-light col-span-2 lg:col-span-4">
-              {formatCurrency(proposal?.penaltyAmount || 0)}
-            </p>
-
-            <p className="pe-2">Bonus amount: </p>
-            <p className="font-light col-span-2 lg:col-span-4">
-              {formatCurrency(proposal?.bonusAmount || 0)}
-            </p>
-
-            <p className="pe-2">Estimated duration: </p>
-            <p className="font-light col-span-2 lg:col-span-4">
-              {proposal?.estimatedDuration} days
-            </p>
-
-            <p className="pe-2">Status: </p>
-            <p className="font-light col-span-2 lg:col-span-4">
-              {proposal?.status}
-            </p>
-
-            <p className="pe-2">Planned Start Date:</p>
-            <p className="font-light col-span-2 lg:col-span-4">
-              {proposal?.plannedStartDate
-                ? formatDate(proposal.plannedStartDate)
-                : "N/A"}
-            </p>
-
-            <p className="pe-2">Planned End Date:</p>
-            <p className="font-light col-span-2 lg:col-span-4">
-              {proposal?.plannedEndDate
-                ? formatDate(proposal.plannedEndDate)
-                : "N/A"}
-            </p>
-
-            <p className="pe-2">Actual Start Date:</p>
-            <p className="font-light col-span-2 lg:col-span-4">
-              {proposal?.actualStartDate
-                ? formatDate(proposal.actualStartDate)
-                : "N/A"}
-            </p>
-
-            <p className="pe-2">Actual End Date:</p>
-            <p className="font-light col-span-2 lg:col-span-4">
-              {proposal?.actualEndDate
-                ? formatDate(proposal.actualEndDate)
-                : "N/A"}
-            </p>
-
-            <p className="pe-2">Created At: </p>
-            <p className="font-light col-span-2 lg:col-span-4">
-              {proposal?.createdAt ? formatDate(proposal.createdAt) : "N/A"}
-            </p>
-
-            <p className="pe-2">Last Update: </p>
-            <p className="font-light col-span-2 lg:col-span-4">
-              {proposal?.lastUpdate ? formatDate(proposal.lastUpdate) : "N/A"}
-            </p>
-
-            <p className="pe-2">Notes:</p>
-            <p className="font-light col-span-2 lg:col-span-4">
-              {proposal?.notes}
-            </p>
-          </section>
-        </div>
-      )}
-
       <div className="space-y-2 p-4 w-full text-start pb-4 text-lg">
         {project && ["COMPLETED", "STOPPED"].includes(project.status!) && (
           <div className="text-red-500 font-medium text-xs text-center">
@@ -263,7 +281,9 @@ const ProposalDetails = ({ id }: ProposalProps) => {
                 className="bg-blue-500 text-gray-200 rounded-sm border border-gray-200 hover:bg-blue-400 w-35"
                 onClick={navigateToEditProposal}
                 disabled={
-                  isLoading ||
+                  isLoadingProposal ||
+                  isProjectLoading ||
+                  isUpdating ||
                   ["COMPLETED", "STOPPED"].includes(project?.status ?? "")
                 }
               >
@@ -278,7 +298,9 @@ const ProposalDetails = ({ id }: ProposalProps) => {
                 className="rounded-sm border border-gray-200 w-35"
                 onClick={handleWithdraw}
                 disabled={
-                  isLoading ||
+                  isLoadingProposal ||
+                  isProjectLoading ||
+                  isUpdating ||
                   ["COMPLETED", "STOPPED"].includes(project?.status ?? "")
                 }
               >
