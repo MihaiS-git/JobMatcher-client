c6cc2b2530a063cc080066642f6e1ee80dfad42c
MihaiS-git <ingsuciumihai@gmail.com>
2025-12-24 18:03:35 +0200

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,8 +4,8 @@ const Footer: React.FC = () => {
     return (
         <footer className="fixed bottom-0 left-0 right-0 w-full h-16 z-40 p-2 text-base text-center font-extrabold bg-blue-600 dark:bg-gray-900 text-blue-100 dark:text-gray-300">
                         <p className="hover:text-red-hover">
-                <Link to="https://mihais-git.github.io/">
-                    Mihai Suciu © 2025
+                <Link to="https://brutecx.vercel.app//">
+                    <span className="text-red-700">Brutecx</span> © 2025
                 </Link>
             </p>
             <span className="font-light text-xs">- Demo project -</span>
